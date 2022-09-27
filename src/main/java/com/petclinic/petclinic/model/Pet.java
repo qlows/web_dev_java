c070331a68e38aa3e196bb4de07efee5808dfcff
qlows <79415956+qlows@users.noreply.github.com>
2022-09-27 08:28:34 -0400

    Closes #2

---
+++ src/main/java/com/petclinic/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package com.petclinic.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private petType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+
+    public com.petclinic.petclinic.model.petType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(com.petclinic.petclinic.model.petType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
